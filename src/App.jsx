15437bfd718179251deece20b689b7635663e0b6
fahrizkhan <fahrizkhan2727@gmail.com>
2026-01-14 13:55:17 +0800

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/* https://localhost:3000/ */}
+            <Route path="/" element={<ListEmployeeComponent />}></Route>
+            {/* https://localhost:3000/employees */}
+            <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
